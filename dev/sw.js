802f3d1a4351a9ad22635d422574519e325b71ca
Joshua Hoke Davis <32313271+jhdavis8@users.noreply.github.com>
2021-04-29 11:44:35 -0400

    Initialize gh-pages

---
+++ dev/sw.js
@@ -0,0 +1,11 @@
+importScripts("/educational-phaser-game-template/devprecache-manifest.9bbb2f9225da99ec00cb98ee8d5d5428.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
